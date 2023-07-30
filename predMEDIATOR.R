76f822ecb4ece53f4af0adb1254874b70f628226
widemberg-nobre <75651911+widemberg-nobre@users.noreply.github.com>
2023-07-30 12:46:58 -0300

    Update predMEDIATOR.R

--- predMEDIATOR.R
+++ predMEDIATOR.R
@@ -71,23 +71,3 @@ dglmNORMAL_FUN <- function(G_med,F_med,discount,idx_discount,M=data.week$age){
 G.sazonal <- function(x){
   return(matrix(c(cos(x),sin(x),-sin(x),cos(x)),2,2,byrow=TRUE))
 }
-omega1 <- 2*pi/52
-omega2 <- 2*pi/26
-
-Gm.ajuste <- matrix(0,7,7)
-
-Gm.ajuste[1,1]     <- 1  
-Gm.ajuste[2:3,2:3] <- diag(2)
-Gm.ajuste[4:5,4:5] <- G.sazonal(omega1)
-Gm.ajuste[6:7,6:7] <- G.sazonal(omega2)
-
-res1.mediator <- dglmNORMAL_FUN(F_med = cbind(1,data.week$gender,data.week$mun_comum,1,0,1,0),
-                          G_med = Gm.ajuste,
-                          discount = c(.98,.995),
-                          idx_discount = list(2,1,2:7))
-
-plot.ts(data.week$age,ylim=c(25,60))
-lines(res1.mediator[,2],col=2,lty=2,lwd=2)
-lines(sqrt(res1.mediator[,3])*1.96 + res1.mediator[,2],col=4,lty=1,lwd=1)
-lines(-sqrt(res1.mediator[,3])*1.96 + res1.mediator[,2],col=4,lty=1,lwd=1)
-
